30c1fe65cf9f57d153db6ed982beabdfb7d13b6e
Huto <92351427+Hutotpn@users.noreply.github.com>
2023-03-11 06:47:57 +0000

    New sign up & Logo bug fixed

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,5 +1,26 @@
-function saved() {
-	setTimeout(() => {
-		location.href = "/comsci-test";
-	}, 1000);
-}
+var signupForm = document.getElementById('signup');
+signupForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  var name = document.getElementById('full-name').value;
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  netlifyIdentity.signup(email, password, {
+    name: name
+  }, function(err) {
+    if (err) {
+      console.error('Error signing up:', err);
+      document.getElementById('signuperr').innerHTML = "Error signing up: " + err
+    } else {
+      console.log('Signed up successfully');
+      netlifyIdentity.login(email, password, function(err) {
+        if (err) {
+          console.error('Error logging in:', err);
+          document.getElementById('signuperr').innerHTML = "Error logging in: " + err
+        } else {
+          console.log('Logged in successfully');
+          window.location.href = '/user.html';
+        }
+      });
+    }
+  });
+});
