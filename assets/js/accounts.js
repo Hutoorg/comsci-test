69abd298ab0218967dc92cff64f5b52697d90218
Huto <92351427+Hutotpn@users.noreply.github.com>
2023-03-24 05:33:59 +0000

    Account 7

--- assets/js/accounts.js
+++ assets/js/accounts.js
@@ -1,5 +1,7 @@
-const auth = netlifyIdentity.init();
-auth.on("login", (user) => {
-  const userName = user.user_metadata.full_name;
-  document.getElementById("welcome").textContent = `Welcome, ${userName}!`;
-});
+setTimeout(() => {
+  const auth = netlifyIdentity.init();
+  auth.on("login", (user) => {
+    const userName = user.user_metadata.full_name;
+    document.getElementById("welcome").textContent = `Welcome, ${userName}!`;
+  });
+}, 1000);
