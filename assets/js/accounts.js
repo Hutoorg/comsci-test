a3cacb9eb67fa4e50b311df2615617b2194413a7
Huto <92351427+Hutotpn@users.noreply.github.com>
2023-03-24 05:45:40 +0000

    Account 8

--- assets/js/accounts.js
+++ assets/js/accounts.js
@@ -1,7 +1,9 @@
 setTimeout(() => {
-  const auth = netlifyIdentity.init();
-  auth.on("login", (user) => {
-    const userName = user.user_metadata.full_name;
-    document.getElementById("welcome").textContent = `Welcome, ${userName}!`;
+  document.addEventListener("DOMContentLoaded", () => {
+    const auth = netlifyIdentity.init();
+    auth.on("login", (user) => {
+      const userName = user.user_metadata.full_name;
+      document.getElementById("welcome").textContent = `Welcome, ${userName}!`;
+    });
   });
 }, 1000);
