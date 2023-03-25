a8e7a1f6e6a9b8dfd9af2a5554062829dfa48a4d
Huto <92351427+Hutotpn@users.noreply.github.com>
2023-03-25 03:56:04 +0000

    Account 12

--- assets/js/accounts.js
+++ assets/js/accounts.js
@@ -1,5 +1,10 @@
 const auth = netlifyIdentity.init();
-auth.on("login", (user) => {
-  const userName = user.user_metadata.full_name;
-  document.getElementById("welcome").textContent = `Welcome, ${userName}!`;
-});
+
+if (auth && typeof auth.on === "function") {
+  auth.on("login", (user) => {
+    const userName = user.user_metadata.full_name;
+    document.getElementById("welcome").textContent = `Welcome, ${userName}!`;
+  });
+} else {
+  console.error("Error: Netlify Identity widget is not loaded properly.");
+}
