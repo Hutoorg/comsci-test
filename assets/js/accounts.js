125875cf9090f513b9a6d02d32d8f42f42a4ce22
Huto <92351427+Hutotpn@users.noreply.github.com>
2023-03-25 07:11:17 +0000

    Style

--- assets/js/accounts.js
+++ assets/js/accounts.js
@@ -1,14 +1,3 @@
-const auth = netlifyIdentity.init();
-
-netlifyIdentity.on("init", () => {
-  // Netlify Identity widget is loaded and ready to use
-});
-
-if (auth && typeof auth.on === "function") {
-  auth.on("login", (user) => {
-    const userName = netlifyIdentity.currentUser().user_metadata.full_name;
-    document.getElementById("welcome").textContent = `Welcome, ${userName}!`;
-  });
-} else {
-  console.error("Error: Netlify Identity widget is not loaded properly.");
-}
+const userMeta = netlifyIdentity.currentUser().user_metadata;
+document.getElementById("welcome").textContent = `Welcome, ${user.full_name}!`;
+document.getElementById("email").textContent = user.email;
